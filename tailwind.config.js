8752b569c9b6d4652497608dd923c05d68c3f664
rhymaado <mightydhiyamatra@yahoo.co.id>
2024-06-12 12:29:14 +0700

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,32 @@ const tailwindConfig = {
     fontSize: {
       "3xl": "1.5rem",
     },
+    colors: {
+      primary: "#61d8de",
+      secondary: "#7c70e6",
+      tertiary: "#e839f6",
+    },
+    screens: {
+      tbt: "426px",
+      sm: "641px",
+      md: "769px",
+      lg: "1025px",
+      xl: "1281px",
+      "2xl": "1537px",
+      uw: "2041px",
+    },
+    extend: {
+      fontSize: {
+        "10xl": "10rem",
+      },
+      gridTemplateColumns: {
+        custom: "100px minmax(100px, 1fr) 200px",
+      },
+      spacing: {
+        halffull: "50%",
+        halfscreen: "50vh",
+      },
+    },
   },
   plugins: [],
 };
